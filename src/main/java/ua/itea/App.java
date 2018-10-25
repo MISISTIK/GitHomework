733804feb49cbe6991ce516cba38043255c416df
Tymoshok Dmytro <dtymochok@auchan.ua>
2018-10-25 13:14:30 +0300

    V2.1 Create the logging into file system

--- src/main/java/ua/itea/App.java
+++ src/main/java/ua/itea/App.java
@@ -1,21 +1,52 @@
 package ua.itea;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Hello world!
  */
 public class App {
+
+    private static PrintWriter pw = null;
+
+    private static void logToFile(String s) {
+        pw.println(s);
+    }
+
+    private static void createLogFile() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMyy");
+        String ldate = dtf.format(LocalDate.now());
+
+        try {
+            if (!Files.exists(Paths.get("logs/log_" + ldate + ".txt"))) {
+                Files.createDirectories(Paths.get("logs"));
+                Files.createFile(Paths.get("logs/log_" + ldate + ".txt"));
+            }
+            pw = new PrintWriter(new FileWriter("logs/log_" + ldate + ".txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void closeLogFile() {
+        if (pw != null) {
+            pw.close();
+        }
+    }
     public static void main(String[] args)
     {
-        if (Files.exists(Paths.get("logs"))) {
-            try {
-                Files.createDirectory(Paths.get("logs"));
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+       createLogFile();
+
+       logToFile("First log write");
+
+       closeLogFile();
     }
 }
