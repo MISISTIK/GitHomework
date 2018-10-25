98337bcc2c8c5e57fd1ebc0203c992d141548eb5
Tymoshok Dmytro <dtymochok@auchan.ua>
2018-10-25 13:27:44 +0300

    V3.2 Add the loging system to main method

--- src/main/java/ua/itea/PudgeFrame.java
+++ src/main/java/ua/itea/PudgeFrame.java
@@ -1,9 +1,46 @@
 package ua.itea;
 
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class PudgeFrame
 {
+    private static PrintWriter pw = null;
+
+    private static void logToFile(String s) {
+        pw.println(s);
+    }
+
+    private static void createLogFile() {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("ddMMyy");
+        String ldate = dtf.format(LocalDate.now());
+
+        try {
+            if (!Files.exists(Paths.get("logs/log_" + ldate + ".txt"))) {
+                Files.createDirectories(Paths.get("logs"));
+                Files.createFile(Paths.get("logs/log_" + ldate + ".txt"));
+            }
+            pw = new PrintWriter(new FileWriter("logs/log_" + ldate + ".txt"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void closeLogFile() {
+        if (pw != null) {
+            pw.close();
+        }
+    }
+
     public static void main( String[] args )
     {
-        System.out.println( "Some changes to check the V3 branch" );
+        createLogFile();
+        logToFile( "Some changes to check the V3 branch" );
+        closeLogFile();
     }
 }
