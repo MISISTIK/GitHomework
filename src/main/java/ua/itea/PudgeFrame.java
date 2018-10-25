f664b472aa67ad06986ded5b5d15d9ecab4b3bdf
Tymoshok Dmytro <dtymochok@auchan.ua>
2018-10-25 13:25:46 +0300

    V3.1 Change the main print string

--- src/main/java/ua/itea/PudgeFrame.java
+++ src/main/java/ua/itea/PudgeFrame.java
@@ -4,6 +4,6 @@ public class PudgeFrame
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Some changes to check the V3 branch" );
     }
 }
