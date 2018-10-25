6cb84295f395c84d14b9fbe3cae91e7e3a1229a8
Tymoshok Dmytro <dtymochok@auchan.ua>
2018-10-25 12:42:19 +0300

    V2.1 Add the Creation of logs folder and exclude it from git. Change pom to 1.8 java version

--- src/main/java/ua/itea/App.java
+++ src/main/java/ua/itea/App.java
@@ -1,13 +1,21 @@
 package ua.itea;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
+public class App {
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        if (Files.exists(Paths.get("logs"))) {
+            try {
+                Files.createDirectory(Paths.get("logs"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
